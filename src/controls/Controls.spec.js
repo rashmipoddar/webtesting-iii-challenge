0c55389803ca48aa82be01122f323a8bf263dd22
Rashmi <poddar.rashmi@gmail.com>
2019-10-14 18:15:53 -0400

    Added snapshot testing

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -1,1 +1,12 @@
 // Test away!
+
+import React from 'react';
+import { render } from '@testing-library/react';
+
+import Controls from './Controls';
+
+describe('<Controls />', () => {
+  it('should match snapshot', () => {
+    expect(render(<Controls />) ).toMatchSnapshot();
+  });
+});
--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,1 +1,11 @@
 // Test away
+import React from 'react';
+import { render } from '@testing-library/react';
+
+import Dashboard from './Dashboard';
+
+describe('<Dashboard />', () => {
+  it('should match snapshot', () => {
+    expect(render(<Dashboard />) ).toMatchSnapshot();
+  });
+});
