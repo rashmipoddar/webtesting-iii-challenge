1146e16730f2b98867a1686fc63196b25c3d2d96
Rashmi <poddar.rashmi@gmail.com>
2019-10-15 11:11:12 -0400

    Added test for controls component

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -1,7 +1,7 @@
 // Test away!
 
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 
 import Controls from './Controls';
 
@@ -9,4 +9,24 @@ describe('<Controls />', () => {
   it('should match snapshot', () => {
     expect(render(<Controls />) ).toMatchSnapshot();
   });
+});
+
+test('toggleClosed is called when close gate button is clicked', () => {
+  const toggleClosedMock = jest.fn();
+  const { getByText } = render(
+    <Controls toggleClosed={toggleClosedMock} />
+  )
+  fireEvent.click(getByText(/close gate/i));
+  expect(toggleClosedMock).toHaveBeenCalled();
+});
+
+test('toggleClosed is called when open gate button is clicked', () => {
+  const toggleClosedMock = jest.fn();
+  const { getByText } = render(
+    <Controls toggleClosed={toggleClosedMock} closed={true} />
+  )
+  // fireEvent.click(getByText(/close gate/i));
+  // expect(toggleClosedMock).toHaveBeenCalled();
+  fireEvent.click(getByText(/open gate/i));
+  expect(toggleClosedMock).toHaveBeenCalled();
 });
